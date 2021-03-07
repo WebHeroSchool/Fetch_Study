c63d8df3d5e631e05facaac2931a4ab5f51f8e37
Александр Ершов <sanya_evr@mail.ru>
2021-03-07 20:46:48 +0600

    Add all files

---
+++ script.js
@@ -0,0 +1,31 @@
+//let login = prompt('Введите логин');
+
+let login = window.location.search.split('=')[1] || 'Alexandr886';
+
+console.log(window.location.search);
+console.log(window.location.search.split('='));
+console.log(login);
+
+fetch(`https://api.github.com/users/${login}`)
+  .then(res => res.json())
+  .then(json => {
+
+    let userName = document.querySelector('.userName');
+    userName.innerHTML = `Пользователь: ${json.name}`;
+    userName.href = json.html_url;
+
+    if (json.bio == null) {
+      document.querySelector('.userDescription').innerHTML = 'Описание пользователя: не заполнено';
+    } else {
+      document.querySelector('.userDescription').innerHTML = `Описание пользователя: ${json.bio}`;
+    }
+
+    let img = new Image();
+    img.src = json.avatar_url;
+    document.body.append(img);
+
+  })
+  .catch(err => {
+    document.querySelector('.userName').innerHTML = `Пользователь не найден: ${err}`;
+    document.querySelector('.userDescription').innerHTML = `Пользователь не найден: ${err}`;
+  })
